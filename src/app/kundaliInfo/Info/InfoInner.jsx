8a972d80c507980d8a88ee184af982488f7cb968
Nitishjhaa <nj9876543210p@gmail.com>
2025-11-20 23:46:08 +0530

    working on the json file

--- src/app/kundaliInfo/Info/InfoInner.jsx
+++ src/app/kundaliInfo/Info/InfoInner.jsx
@@ -94,7 +94,9 @@ export default function KundaliInfoInner() {
 
                 </div>
             </div>
-
+            <pre className="whitespace-pre-wrap bg-black/40 p-3 rounded-xl text-white">
+                {JSON.stringify(kundali.raw, null, 2)}
+            </pre>
         </div>
     );
 }
